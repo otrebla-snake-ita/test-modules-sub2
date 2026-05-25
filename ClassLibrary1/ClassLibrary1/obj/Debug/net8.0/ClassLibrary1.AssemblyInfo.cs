7038b4069c15b96175cf7455313c23e5a7bcc6c9
Alberto Marinucci <alberto.marinucci@insoore.com>
2026-05-25 17:09:41 +0200

    Added submodule

--- ClassLibrary1/ClassLibrary1/obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/ClassLibrary1/obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74cca6348ebad2eadc94c5db03f59cd0bedcc81f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
